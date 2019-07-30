16691635710a4f141072b8c0282fdef538ddea97
Kai <thiskaiguy@gmail.com>
2019-07-30 10:39:11 +0100

    Create tests for struct and enum default values

--- tests/constructor_macro.rs
+++ tests/constructor_macro.rs
@@ -55,6 +55,102 @@ fn default_value_attribute() {
 }
 
 #[test]
+fn default_value_unit_struct() {
+    struct Value;
+
+    #[derive(Debug, PartialEq, ConstructorMacro)]
+    struct DefaultValueUnitStruct {
+        #[default = Value]
+        field1: Value,
+    }
+
+    let thing = DefaultValueUnitStruct!();
+
+    assert_eq!(thing.field1, Value);
+}
+
+#[test]
+fn default_value_tuple_struct() {
+    struct Value(usize);
+
+    #[derive(Debug, PartialEq, ConstructorMacro)]
+    struct DefaultValueTupleStruct {
+        #[default = Value(0)]
+        field1: Value,
+    }
+
+    let thing = DefaultValueTupleStruct!();
+
+    assert_eq!(thing.field1, Value(0));
+}
+
+#[test]
+fn default_value_struct() {
+    struct Value{ value: usize };
+
+    #[derive(Debug, PartialEq, ConstructorMacro)]
+    struct DefaultValueStruct {
+        #[default = Value { value: 0 }]
+        field1: Value,
+    }
+
+    let thing = DefaultValueStruct!();
+
+    assert_eq!(thing.field1, Value { value: 0 });
+}
+
+#[test]
+fn default_value_c_enum() {
+    enum Either {
+        Left,
+        Right,
+    }
+    #[derive(Debug, PartialEq, ConstructorMacro)]
+    struct DefaultValueCEnum {
+        #[default = Either::Left]
+        field1: Either,
+    }
+
+    let thing = DefaultValueCEnum!();
+
+    assert_eq!(thing.field1, Either::Left);
+}
+
+#[test]
+fn default_value_enum_field() {
+    enum Either {
+        Left(usize),
+        Right(usize),
+    }
+    #[derive(Debug, PartialEq, ConstructorMacro)]
+    struct DefaultValueEnumField {
+        #[default = Either::Left(0)]
+        field1: Either,
+    }
+
+    let thing = DefaultValueEnumField!();
+
+    assert_eq!(thing.field1, Either::Left(0));
+}
+
+#[test]
+fn default_value_enum_named_field() {
+    enum Either {
+        Left { value: usize },
+        Right { value: usize },
+    }
+    #[derive(Debug, PartialEq, ConstructorMacro)]
+    struct DefaultValueEnumNamedField {
+        #[default = Either::Left { value: 0 }]
+        field1: Either,
+    }
+
+    let thing = DefaultValueEnumNamedField!();
+
+    assert_eq!(thing.field1, Either::Left { value: 0 });
+}
+
+#[test]
 fn unit_struct() {
     #[derive(ConstructorMacro)]
     struct EmptyThing;
